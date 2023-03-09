a9f946532e86216223c237fbb3dcb26dd37fbf5c
Helina A. Gebreyes <helinaago@gmail.com>
2023-03-09 18:14:54 +0300

    task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -14,8 +14,7 @@ request(url, async (err, res, body) => {
     await new Promise((resolve, reject) => {
       request(character, (err, res, body) => {
         err && console.log(err);
-        
-	console.log(JSON.parse(body).name);
+        console.log(JSON.parse(body).name);
         resolve();
       });
     });
